a6f78565e80e866487d2fd8bf7a8617602071a55
MALockwood <marklockwood85@gmail>
2024-05-14 01:57:57 +0100

    created final objects for computer dispenser & shefl/draw, added logic to check for bnf labels and print to label if in required range

--- Assets/Scripts/LabelProperties.cs
+++ Assets/Scripts/LabelProperties.cs
@@ -1,4 +1,7 @@
 using Oculus.Interaction.HandGrab;
+using System;
+using System.Collections.Generic;
+using System.Linq;
 using TMPro;
 using UnityEngine;
 
@@ -6,10 +9,12 @@ public class LabelProperties : MonoBehaviour
 {
     [SerializeField] 
     private TMP_Text patientNameText, todaysDateText, 
-    medicationText, frequencyText;
+    medicationText, frequencyText, bnfText;
+
+    private List<BnfLabel> bnfData;
 
     public string patientName, todaysDate, medicationName,
-    doseage, frequency;
+    doseage, frequency, bnfLabel;
 
     public StrengthUnit strengthUnit;
     public MedicationType medicationType;
@@ -18,18 +23,82 @@ public class LabelProperties : MonoBehaviour
 
     public int quantity;
 
-    private bool grabInteraction;
+    //private bool grabInteraction;
 
     [SerializeField]
     private HandGrabInteractable _interactable;
 
-    //public LabelProperties(string name, string freq)
-    //{
-    //    //this._medicationName = name;
-    //    //this.frequency = freq;
-    //    _medicationName = name;
-    //    frequency = freq;
-    //}
+    public GameObject prescription;
+
+    void Awake()
+    {
+        CheckForBnf();
+        
+
+        patientNameText.text = PatientName;
+        todaysDateText.text = TodaysDate;
+        medicationText.text = Quantity + " " + MedicationName + " " + Strength + " " + StrengthUnit + " " + MedicationType;
+        frequencyText.text = "Take " + Dosage.ToUpper() + " " + Frequency;
+
+    }
+
+    private void CheckForBnf()
+    {
+        bnfData = ReadCSV.readBnfData();
+        
+        Debug.Log("BNF Labels: " + bnfData.Count);
+
+        foreach (var bnfData in bnfData)
+        {
+            Debug.Log("BNF: " + bnfData.Label);
+        }
+        
+
+        prescription = GameObject.Find("Prescription");
+
+        PrescriptionProperties prescriptionProperties = new PrescriptionProperties();
+        prescriptionProperties = prescription.GetComponentInChildren<PrescriptionProperties>();
+
+        string bnfLabels = prescriptionProperties.prescription.BnfLabels;
+
+        if (bnfLabels.Length <= 0)
+        {
+            bnfText.text = "Empty";
+        }
+        else
+        {
+            // String separating characters
+            string[] separatingStrings = { ";", ";;" };
+
+            //string BNFLabels = "2;20;,;;f;2null;'2null';25";
+
+            //creates a string array "words" by spliting on the chosen characters also removing empty entries
+            string[] words = bnfLabels.Split(separatingStrings, System.StringSplitOptions.RemoveEmptyEntries);
+
+            int i = 0;
+            // iterates through each element s in the words array, using TryParse if the string is an int
+            // it stores it to the 'a' int[]
+            int[] a = (from s in words where int.TryParse(s, out i) select i).ToArray();
+
+            foreach( int n in a )
+            {
+                if (n >= 21 && n <= 28)
+                {
+                    int labelNum = 0;
+                    labelNum = n;
+                    Debug.Log("BNF Labels num : " + labelNum);
+
+                    BnfLabel bnf = bnfData.Find(b => b.Number.Equals(labelNum));
+
+                    bnfText.text = bnf.Label;
+                }
+
+            }
+
+        }
+        Debug.Log("BNF Labels: " +  bnfLabels + " Length " + bnfLabels.Length);
+        
+    }
 
     public string PatientName
     {
@@ -93,34 +162,12 @@ public class LabelProperties : MonoBehaviour
         set { frequency = value;}
     }
 
-
-    // Start is called before the first frame update
-    void Awake()
+    public string BnfLabel
     {
-        patientNameText.text = PatientName;
-        todaysDateText.text = TodaysDate;
-        medicationText.text = Quantity + " " + MedicationName + " " + Strength + " " + StrengthUnit + " " + MedicationType;
-        frequencyText.text = "Take " + Dosage.ToUpper() + " " + Frequency;
+        get { return bnfLabel; }
+        // restrict changes to the data 
+        set { bnfLabel = value; }
+
     }
 
-    // Update is called once per frame
-    void Start()
-    {
-        //GrabInteractable _interactable = transform.GetComponent<GrabInteractable>();
-
-        //var grab = _interactable.Interactors.FirstOrDefault<HandGrabInteractor>();
-
-        ////grabInteraction = _interactable.ResetGrabOnGrabsUpdated;
-        
-        ////Debug.Log("Grab " + grab);
-
-        //if (grab == null) 
-        //{
-        //    Debug.Log("Grab released");
-        //} 
-        //else if (grab != null)
-        //{
-        //    Debug.Log("Grab grabed");
-        //}
-    }
 }
